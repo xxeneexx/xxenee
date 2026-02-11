6b250889522d600c5f1e5a72ac85395971527a8e
xxeneexx <rlapdls123@gmail.com>
2026-02-11 21:12:47 +0900

    20260211

---
+++ 04_클래스/mod1.py
@@ -0,0 +1,13 @@
+def add(a,b)
+print("현재파일이름",__name__)
+    return a + b
+
+def sub(a,b)
+    return a - b
+
+
+if __name__ == ""__main__":
+    print(add(1,4))
+    print(sub(4,2))
+
+    #터미널에 뭘호출해야함
